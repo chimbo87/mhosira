1676035b1fd4193952eef3a2bdd289d0d1645c70
Archford Nhanga <archfordnhanga@gmail.com>
2025-01-17 11:30:57 +0200

    designs

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,16 +4,18 @@ import { GrLocation } from "react-icons/gr";
 import { HiOutlineMail } from "react-icons/hi";
 import { MdOutlinePhone } from "react-icons/md";
 import { FiFacebook } from "react-icons/fi";
+import { SlUserFollow } from "react-icons/sl";
 import { FaInstagram } from "react-icons/fa";
 import { RiTiktokLine } from "react-icons/ri";
 import { FaWhatsapp } from "react-icons/fa";
+import userlogo from "../../assets/logo2.png";
 
 function Footer() {
   return (
     <div className="container-fluid" id="footer-page">
       <h4>Get In Touch With Us</h4>
       <div class="row">
-        <div class="col-lg-4 col-md-4">
+        <div class="col-lg-3 col-md-4">
           <div id="footer-page-card">
             <h5>
               <GrLocation /> Address
@@ -23,42 +25,53 @@ function Footer() {
             </p>
           </div>
         </div>
-        <div class="col-lg-4 col-md-4">
+        <div class="col-lg-3 col-md-4">
           <div id="footer-page-card">
             <h5>
               <HiOutlineMail /> Emails
             </h5>
             <p>
-              test@example.com<br></br>
-              test@example.com<br></br> test@example.com
+              mobilelandx@gmail.com<br></br>
+              mhosirax@gmail.com<br></br>
             </p>
           </div>
         </div>
-        <div class="col-lg-4 col-md-4">
+        <div class="col-lg-3 col-md-4">
           <div id="footer-page-card">
             <h5>
               <MdOutlinePhone /> Phone
             </h5>
             <p>
               +26377 360 9855<br></br> +26371 367 6303 <br></br> +26377 508 0424
-             
             </p>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-4">
+          <div id="footer-page-card">
+            <h5>
+              <SlUserFollow /> Follow Us
+            </h5>
+            <img src={userlogo} alt="logo" />
+            <div>
+              <FiFacebook id="footer-icon" />
+              <FaInstagram id="footer-icon" />
+              <RiTiktokLine id="footer-icon" />
+              <FaWhatsapp id="footer-icon" />
+            </div>
           </div>
         </div>
       </div>
       <div id="footer-icons-cards" className="container">
-        <div>
-          <FiFacebook id="footer-icon"/>
-          <FaInstagram id="footer-icon"/>
-          <RiTiktokLine id="footer-icon"/>
-          <FaWhatsapp id="footer-icon"/>
-        </div>
+        <p style={{color:"white", marginTop:"10px"}}> &copy; Mobileland 2025</p>
         <div id="footer-rtys">
           <small>
-            {" "}
-            &copy; Mobileland 2025. Crafted by{" "}
-            <a href="https://wa.me/27677366141" target="_blank" style={{color:"#fff"}}>
-              chimbo
+            Crafted by:
+            <a
+              href="https://wa.me/27677366141"
+              target="_blank"
+              style={{ color: "#f1975b", marginLeft:"5px"}}
+            >
+             CHIMBO
             </a>
           </small>
         </div>
