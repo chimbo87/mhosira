fedb13d2f510900a37ad3a4be2b7e19124e0d1ca
Archford Nhanga <archfordnhanga@gmail.com>
2025-01-25 19:21:46 +0200

    designs-fix

--- src/components/shop/Shop.jsx
+++ src/components/shop/Shop.jsx
@@ -146,49 +146,39 @@ function Shop() {
 
   return (
     <div className="container" id="shop-page">
-      <h4>Check out our latest products</h4>
-      <Swiper
-        modules={[Pagination, Autoplay]}
-        spaceBetween={30}
-        slidesPerView={1}
-        pagination={{ clickable: true }}
-        autoplay={{
-          delay: 2500,
-          disableOnInteraction: false,
-        }}
-        breakpoints={{
-          // when window width is >= 768px
-          768: {
-            slidesPerView: 2,
-          },
-          // when window width is >= 1024px
-          1024: {
-            slidesPerView: 3,
-          },
-        }}
-      >
-        {Products.map((product, index) => (
-          <SwiperSlide key={index}>
-            <div
-              id="shop-page-card"
-              onClick={() => {
-                navigate("/our-shop");
-              }}
-            >
-              <img src={product.image} alt={product.title} />
-              <Button>View more</Button>
-              {/* <p>  {product.productName}</p>
-              <div id="shop-card-price">
-                <p id="shop-card-priceA"> was ${product.oldPrice.toFixed(2)}</p>
-                <p id="shop-card-priceB">
-                now ${product.price.toFixed(2)}
-                </p>
-              </div> */}
-            </div>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </div>
+    <h4>Check out our latest products</h4>
+    <Swiper
+      modules={[Autoplay]}
+      spaceBetween={30}
+      slidesPerView={1}
+      autoplay={{
+        delay: 2500,
+        disableOnInteraction: false,
+      }}
+      breakpoints={{
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+      }}
+    >
+      {Products.map((product, index) => (
+        <SwiperSlide key={index}>
+          <div
+            id="shop-page-card"
+            onClick={() => {
+              navigate("/our-shop");
+            }}
+          >
+            <img src={product.image} alt={product.title} />
+            <Button>View more</Button>
+          </div>
+        </SwiperSlide>
+      ))}
+    </Swiper>
+  </div>
   );
 }
 
